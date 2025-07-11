0c86dd543f5f9273adb8ba54ed20a7f27eecfc2d
rahulambhore21 <ambhorerahul69@gmail.com>
2025-07-11 17:28:30 +0530

    try deploy 03 frontend

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || "http://localhost:3000" || '*',
+    origin:'*',
     methods: ["GET", "POST"],
     credentials: true
   }
